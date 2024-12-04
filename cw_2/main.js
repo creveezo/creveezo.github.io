900c49dc7a6e214d9cf81c1b059a7e1be31d7b38
creveezo <chernovaolga66@gmail.com>
2024-12-04 03:16:10 +0300

    сортировка

--- cw_2/main.js
+++ cw_2/main.js
@@ -1,32 +1,52 @@
+let content = [];
+
 async function getResponse() {
-    // let responce = await fetch("https://my-json-server.typicode.com/typicode/demo/posts")
-    //let responce = await fetch("https://vmarshirov.github.io/g06u28/030_js/data/0620.json")
-    //let responce = await fetch("http://185.182.111.214:7628/tmp/g06u28.txt_api.json")
-//    let responce = await fetch("../../tmp/g06u28.shop.json")
-    let response = await fetch("../cw_2/shoping.json")
-    console.log("response:\n", response, "\n /response: \n")
-    let content = await response.text()
-    console.log("await response.text()\n", content)
-    content = JSON.parse(content)
-    content = content.slice(0, 9)
-    console.log("content.slice(0, 9)", content)
-    let key
-    for (key in content) {
-        console.log(content[key].id, content[key].title)
-        console.log(content[key])
+    let response = await fetch("../cw_2/shoping.json");
+    console.log("response:\n", response, "\n /response: \n");
+    let data = await response.text();
+    console.log("await response.text()\n", data);
+    content = JSON.parse(data);
+    content = content.slice(0, 9);
+    console.log("content.slice(0, 9)", content);
+    
+    renderProducts(content);
+}
+
+function renderProducts(products) {
+    let node_for_insert = document.getElementById("node_for_insert");
+    node_for_insert.innerHTML = '';
+    products.forEach(item => {
+        node_for_insert.innerHTML += `
+            <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
+                <img style="width: 180px" class="align-self-center" src="${item.img}">
+                <h5 class="card-title">${item.title}</h5>
+                <p class="card-text">${item.description}. Цена ${item.price} р.</p>
+                <input type="hidden" name="vendor_code" value="${item.vendor_code}">
+                <p class="card-text">Заказать <input class="w-25" type="number" name="amount" value="0"></p>
+            </li>
+        `;
+    });
+}
+
+document.getElementById("searchInput").addEventListener("input", function() {
+    let searchValue = this.value.toLowerCase();
+    let filteredContent = content.filter(item => 
+        item.title.toLowerCase().includes(searchValue)
+    );
+    renderProducts(filteredContent);
+});
+
+document.getElementById("sortSelect").addEventListener("change", function() {
+    let sortBy = this.value;
+    let sortedContent = [...content];
+
+    if (sortBy === "price") {
+        sortedContent.sort((a, b) => a.price - b.price);
+    } else if (sortBy === "title") {
+        sortedContent.sort((a, b) => a.title.localeCompare(b.title));
     }
 
-    let node_for_insert = document.getElementById("node_for_insert")
-    for (key in content) {
-        node_for_insert.innerHTML += `
-        <li style="width: 210px" class="d-flex flex-column m-1 p-1 border bg-body">
-        <img style="width: 180px" class="align-self-center" src=${content[key].img}>
-        <h5 class="card-title">${content[key].title}</h5>
-        <p class="card-text">${content[key].description}. Цена ${content[key].price} р.</p>
-        <input type="hidden" name= "vendor_code" value=${content[key].vendor_code}>
-        <p class="card-text" >Заказать <input class="w-25" type="number" name="amount" value="0"></p>
-        </li>
-                `
-    }
-}
-getResponse()
+    renderProducts(sortedContent);
+});
+
+getResponse();
